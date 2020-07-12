0a260eb24944cb60aa8551df09a5d9eec3564072
manoj-kumar-vst-au4 <mauryamanoj513@gmail.com>
2020-07-12 23:43:55 +0530

    small update

--- src/component/HourForcast.js
+++ src/component/HourForcast.js
@@ -4,35 +4,24 @@ import '../../node_modules/react-linechart/dist/styles.css';
  
 export default class HourForcast extends Component {
     render() {
-        const getTime =(timestamp)=>{
-            var date = new Date(timestamp * 1000);
-            var hours = date.getHours();
-            var minutes = "0" + date.getMinutes();
-            var formattedTime = hours + ':' + minutes.substr(-2) ;
-            return formattedTime;
-        }
 
         let Points = this.props.hourlyData.map((data, index)=>{
             return (
                 {
-                    x:parseInt(getTime(data.dt)),
+                    x:parseInt(index),
                     y: parseInt(data.temp - 273.15)
                 }
             );
         });
 
-        let xData = Points.map(data=>{
-            return data.x;
-        })
-
         const data = [
             {									
                 color: "red", 
-                points: Points.slice(0,7),
+                points: Points.slice(0,10),
             }
         ];
 
-        
+        console.log(Points)
         return (
             <div>
                 <div className="App">
@@ -44,7 +33,6 @@ export default class HourForcast extends Component {
                         yLabel=""
                         yMin="0"
                         yMax="50"
-                        xMin={xData[0]}
                         
                     />
                 </div>				
